402394b2608cf5b3568d6941cea258229c1aff09
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2025-05-15 22:11:51 -0300

    init menu

--- PilhasDeBolas/src/pilha/StackExeption.java
+++ PilhasDeBolas/src/pilha/StackExeption.java
@@ -3,18 +3,24 @@ package pilha;
 public class StackExeption extends Exception {
 	
 	private static final long serialVersionUID = 1L;
-	
+	private final String type;
 	String message;
 
-	public StackExeption(String message) {
+	public StackExeption(String message ,String type) {
 		super(message);
+		this.type =type;
 	}
 	
 	public static StackExeption stackIsEmpty(String message) {
-		return new StackExeption(message);
+		return new StackExeption(message,"EMPTY");
 	}
 	
 	public static StackExeption stackItsFull(String message) {
-		return new StackExeption(message);
+		return new StackExeption(message,"FULL");
 	}
+
+	public String getType() {
+		return type;
+	}
+	
 }
---
+++ PilhasDeBolas/src/progam/ShowMenu.java
@@ -0,0 +1,5 @@
+package progam;
+
+public class ShowMenu {
+
+}
